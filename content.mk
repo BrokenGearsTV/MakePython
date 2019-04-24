6f25c799cae0257e784c9f57ac209b77535209c6
BrokenGearsTV <48889543+BrokenGearsTV@users.noreply.github.com>
2019-04-24 13:46:13 +0200

    Update content.mk

--- content.mk
+++ content.mk
@@ -34,38 +34,3 @@ PF_LDSO = -L$(build_dir) -lch-pf
 
 LIBXML_LDSO = -L ./libxml/lib -lxml
 
-CH_COMMON_LDSO = $(PF_LDSO) \
-                 $(LIBXML_LDSO)
-
-ch = $(CH_COMMON) \
-     main/src/ch_gvd_dummy.c \
-     main/src/ch_main.c
-
-# define relied so path for bin target
-# keep the var name as bin_LDSO
-ch_LDSO = $(CH_COMMON_LDSO)
-
-libch-pf.so = $(UTIL) \
-              pf/src/ch_pf_info.c
-
-LIB_COMMON = $(UTIL) \
-             lib/src/ch_lib_notify.c \
-             lib/src/ch_lib_main.c
-
-libch-notify.so = $(LIB_COMMON)
-
-libch.so = $(LIB_COMMON) \
-           $(CH_COMMON)
-
-GVD_COMMON = gvd/gvd/gvd.c
-
-ch_gvd = $(CH_COMMON) \
-         main/src/ch_main.c \
-         gvd/ch/ch_gvd.c
-
-ch_gvd_LDSO = $(CH_COMMON_LDSO)
-
-gvd_proc = $(GVD_COMMON)
-
-gvd_lib = $(GVD_COMMON)
-
